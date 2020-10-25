1d6b89a0f7b66046b21d1da233ac27a6235bd6b9
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-10-25 18:39:27 -0500

    Part 3 completed

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -16,17 +16,21 @@ let hoursToMars: number = milesToMars / speedMph;
 let daysToMars: number = hoursToMars / 24;
 
 // Code an output statement here (use a template literal):
-console.log(`${spacecraftName} would take ${daysToMars} days to get to Mars.`);
+
 
 
 // Part 3: Create a Function ("getDaysToLocation")
-
+function getDaysToLocation(kilometersAway: number): number {
+    let milesAway: number = kilometersAway * milesPerKilometer;
+    let hoursToLocation: number = milesAway / speedMph;
+    return hoursToLocation / 24;
+};
 
 
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
-
-
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToMars)} days to get to Mars.`);
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToTheMoon)} days to get to Mars.`);
 
 
 // Part 4: Create a Spacecraft Class
