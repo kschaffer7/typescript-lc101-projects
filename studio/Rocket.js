c0d8ebda21f137095f95e60a565871ed50fc2f59
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-10-27 19:10:26 -0500

    all steps completed

--- studio/Rocket.js
+++ studio/Rocket.js
@@ -1,57 +1,49 @@
-import { Payload } from './Payload';
-import { Cargo } from './Cargo';
-import { Astronaut } from './Astronaut';
-
-export class Rocket implements Payload {
-    name: string;
-    totalCapacityKg: number;
-    cargoItems: Cargo[] = [];
-    astronauts: Astronaut[] = [];
-    massKg: number;
-
-    constructor(name: string, totalCapacityKg: number) {
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
         this.name = name;
         this.totalCapacityKg = totalCapacityKg;
-
     }
-
     // Method returns the sum of all items using each item's massKg property
-    sumMass(items: Payload[]): number {
-        let sum: number = 0;
-        for (let i = 0; i < items.length; i++) {
+    Rocket.prototype.sumMass = function (items) {
+        var sum = 0;
+        for (var i = 0; i < items.length; i++) {
             sum += items[i].massKg;
         }
         return sum;
-    }
-
+    };
     // Method uses this.sumMass to return the combined mass of this.astronauts and this.cargoItems
-    currentMassKg(): number {
+    Rocket.prototype.currentMassKg = function () {
         return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
-    }
-
+    };
     // Method returns true if this.currentMassKg() + item.massKg <= this.totalCapacityKg
-    canAdd(item: Payload): boolean {
+    Rocket.prototype.canAdd = function (item) {
         return (this.currentMassKg() + item.massKg) <= this.totalCapacityKg;
-    }
-
+    };
     // Method uses this.canAdd() to see if another item can be added.
-    addCargo(cargo: Cargo): boolean {
+    Rocket.prototype.addCargo = function (cargo) {
         if (this.canAdd(cargo)) {
             this.cargoItems.push(cargo);
             return true;
-        } else {
+        }
+        else {
             return false;
         }
-    }
-
+    };
     // Method Uses this.canAdd() to see if another astronaut can be added
-    addAstronaut(astronaut: Astronaut): boolean {
+    Rocket.prototype.addAstronaut = function (astronaut) {
         if (this.canAdd(astronaut)) {
             this.astronauts.push(astronaut);
             return true;
-        } else {
+        }
+        else {
             return false;
         }
-    }
-
-}
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
